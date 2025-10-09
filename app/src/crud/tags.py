299e61c6c9013e4e4207defb1f7b081e31acdaa0
Adam Chandler <adam.c02pro@gmail.com>
2025-10-09 11:34:28 +1030

    feat: adding tag deletion logic

--- app/src/crud/tags.py
+++ app/src/crud/tags.py
@@ -36,6 +36,11 @@ def create_tag(metadata_key: str, new_tag_val: str, redis_client: Redis):
         object_id = metadata_key.removeprefix("metadata:")
         tag_key = f"tag:{new_tag_val}"
         redis_client.sadd(tag_key, object_id)
+
+        existing_tags.append(new_tag_val)
+        updated_tags_json = json.dumps(existing_tags)
+
+        redis_client.hset(metadata_key, "tags", updated_tags_json)
     else:
         logger.warning(
             f"Tag: {new_tag_val} is already attached to item: {metadata_key} \n skipping tag."
@@ -43,4 +48,30 @@ def create_tag(metadata_key: str, new_tag_val: str, redis_client: Redis):
         return True
 
     logger.info(f"Tag: {new_tag_val} successfully attached to item: {metadata_key}.")
+    return True
+
+
+def remove_tag(metadata_key: str, tag_value: str, redis_client: Redis):
+    logger.info(f"Removing tag: {tag_value} from item {metadata_key}")
+
+    try:
+        tags_retrieved = retrieve_tags(metadata_key, redis_client)
+    except ValueError:
+        logger.error("Exiting tag deletion routine")
+        return False
+
+    existing_tags = [] if tags_retrieved is None else json.dumps(tags_retrieved)
+
+    if tag_value not in existing_tags:
+        logger.error(f"Tag: {tag_value} is not attached to item {metadata_key}")
+        return True
+
+    existing_tags.remove(tag_value)
+    updated_tag_json = json.dumps(existing_tags)
+
+    redis_client.srem(f"tag:{tag_value}", metadata_key.removeprefix("metadata:"))
+    logger.info(f"Tag: {tag_value} removed from reverse lookup of item: {metadata_key}")
+
+    redis_client.hset(metadata_key, "tags", updated_tag_json)
+    logger.info(f"{tag_value} removed from item: {metadata_key} successfully.")
     return True
